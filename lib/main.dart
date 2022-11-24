3a6132be4ac34efdc06d9b4ebe8d507161000c2a
sweeetlatte <thuylinh271055@gmail.com>
2022-11-24 14:19:56 +0700

    feat: UI task

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xfffde9c9),
       appBar: AppBar(
         title: const Text('Todo List',
             style: TextStyle(fontSize: 40, fontWeight: FontWeight.w300)),
-        backgroundColor: const Color.fromRGBO(13, 13, 13, 1),
+        backgroundColor: const Color(0xff0d0d0d),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 74,
+              margin: EdgeInsets.only(bottom: 12),
+              decoration: BoxDecoration(
+                border: Border.all(width: 1.5, color: const Color(0xff0d0d0d)),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text('Do the homework'),
+                    Icon(
+                      Icons.delete,
+                      color: Color(0xff0d0d0d),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromRGBO(13, 13, 13, 1),
+        backgroundColor: const Color(0xff0d0d0d),
         child: const Icon(Icons.add),
         onPressed: () {},
       ),
