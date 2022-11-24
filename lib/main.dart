b669bfe1bced0474f53453e7c29cc41d0c5448fe
sweeetlatte <thuylinh271055@gmail.com>
2022-11-24 13:51:32 +0700

    feat: UI appBar + FAC

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo List'),
+        title: const Text('Todo List',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w300)),
+        backgroundColor: const Color.fromRGBO(13, 13, 13, 1),
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color.fromRGBO(13, 13, 13, 1),
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
